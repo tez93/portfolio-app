03f17c7081a8cbe05e3db942dcb772585c6886a6
Tejas Rao <tejasgrock@gmail.com>
2026-08-11 00:15:43 +0530

    Remove nested Router to resolve duplicate router error

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-
-// ADD THIS LINE BELOW:
-import Home from './components/pages/Home'; 
-
+import Home from './components/pages/Home';
 import './App.css';
 
 function App() {
