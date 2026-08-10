f35618a90afddce876b6cfea4a69788f0c18dc0d
Tejas Rao <tejasgrock@gmail.com>
2026-08-10 23:37:01 +0530

    Fixed and added about info and arranged skills card with spacing

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,15 +1,14 @@
-import React from "react";
+import React from 'react';
 
-
-export default function SkillCard({ category, skills }){
-    return (
-        <div>
-            <h3>{category}</h3>
-            <ul>
-                {skills.map((skills, index) => (
-                    <li key={index}>{skills}</li>
-                ))}
-            </ul>
-        </div>
-    )
+export default function SkillCard({ category, skills }) {
+  return (
+    <div className="skill-category-card">
+      <h3>{category}</h3>
+      <ul>
+        {skills.map((skill, index) => (
+          <li key={index}>{skill}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
