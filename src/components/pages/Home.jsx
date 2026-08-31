71ca99e54ce2cfb76efb75d962c5c8e5cd175f85
Tejas Rao <tejasgrock@gmail.com>
2026-08-31 22:55:21 +0530

    updated new resume

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,7 +6,8 @@ import passportImg from "../../assets/images/passport.jpg";
 import udemyImg from "../../assets/images/udemy.jpg";
 import testcase from "../../assets/images/testcase.jpg";
 import devops from "../../assets/images/devops.jpg";
-import Tejas_G_Rao_React_Developer_Resume from "../../assets/Tejas_G_Rao_React_Developer_Resume.pdf";
+import Awsconnect from "../../assets/images/Awsconnect.webp";
+import TEJAS_G_RAO_SOFTWARE_ENGINEER_RESUME from "../../assets/TEJAS G RAO_SOFTWARE_ENGINEER_RESUME.pdf";
 
 // Import Components
 import Navbar from "../Navbar";
@@ -43,22 +44,20 @@ export default function Home() {
           />
           <div>
             <p>
-              Software Engineer with 2 years of experience in backend
-              application development using Java, Spring Boot, REST APIs,
-              PostgreSQL, Azure DevOps, and Microservices. Experienced in
-              designing scalable backend solutions, collaborating with
-              cross-functional teams, and delivering enterprise applications.
+              Software Engineer with 2+ years of experience in backend and
+              frontend development using Java, Spring Boot, REST APIs,
+              Microservices, PostgreSQL, React.js, JavaScript, and Azure DevOps.
+              Experienced in building scalable enterprise applications,
+              responsive user interfaces, and collaborating with
+              cross-functional teams. Expanding into full-stack development
+              through self-directed learning, mastering HTML, CSS, JavaScript
+              (ES6+), and React to build dynamic, responsive web applications
+              like this portfolio.
             </p>
             <p>
-              Expanding into full-stack development through self-directed
-              learning, mastering HTML, CSS, JavaScript (ES6+), and React to
-              build dynamic, responsive web applications like this portfolio.
-            </p>
-            <p>
-              Additional exposure to healthcare operations and US revenue cycle
-              management, strengthening business process understanding and
-              stakeholder communication. Passionate about building reliable
-              software solutions and continuously learning modern technologies.
+              Currently learning AWS Amazon Connect and cloud-based contact
+              center technologies, with a focus on IVR, contact flows, call
+              routing, queues, and AWS service integrations.
             </p>
           </div>
         </section>
@@ -67,7 +66,10 @@ export default function Home() {
         <section id="skills">
           <SectionHeader title="Skills" />
           <div id="skills-grid">
-            <SkillCard category="Programming" skills={["Java", "SQL"]} />
+            <SkillCard
+              category="Programming"
+              skills={["Java", "SQL", "Python"]}
+            />
             <SkillCard
               category="Backend"
               skills={["Spring Boot", "REST APIs", "Microservices"]}
@@ -79,7 +81,14 @@ export default function Home() {
             <SkillCard category="Database" skills={["PostgreSQL", "MySQL"]} />
             <SkillCard
               category="DevOps"
-              skills={["Azure DevOps", "Docker", "CI/CD", "Git"]}
+              skills={[
+                "Azure DevOps",
+                "Docker",
+                "CI/CD",
+                "Git",
+                "AWS",
+                "AWS CONNECT",
+              ]}
             />
             <SkillCard category="Messaging" skills={["Kafka", "RabbitMQ"]} />
             <SkillCard
@@ -93,15 +102,15 @@ export default function Home() {
         <section id="professional-experience">
           <SectionHeader title="Professional Experience" />
           <JobCard
-            title="Accounts Receivable Specialist – Healthcare Operations"
+            title="Accounts Receivable Support Specialist – Healthcare Operations"
             date="March 2025 – May 2026"
             company="3Gen Consulting Services Pvt. Ltd"
-            description="Managed healthcare insurance claims and denial resolution for US healthcare clients in the DME
-billing domain.
-Performed eligibility verification and claim follow-ups with insurance providers to ensure timely
-reimbursements.
-Analyzed claim statuses and payment trends to support efficient revenue cycle management.
-Collaborated with insurance representatives to resolve billing discrepancies and claim denials.
+            description="Utilized Azure DevOps CI/CD pipelines to automate build and deployment processes. 
+            Designed and optimized database schemas using PostgreSQL.
+            Implemented messaging integration using Kafka and RabbitMQ for distributed system
+communication.
+Analyzed claim statuses and payment trends to support efficient revenue cycle management .
+Collaborated with insurance clients to resolve billing discrepancies and claim denials on the Health care systems Using REST API's.
 Maintained HIPAA compliance while handling sensitive patient data"
           />
           <JobCard
@@ -122,13 +131,6 @@ using Mockito while monitoring code coverage with JaCoCo.
 Participated in requirement analysis, sprint planning, code reviews, and continuous process
 improvements within the software development lifecycle.
 "
-          />
-
-          <JobCard
-            title="Junior System Administrator  L1 - Chat Support (Part -Time)"
-            date="March 2021 – Nov 2021"
-            company="Diya Systems Pvt Ltd"
-            description="Provided technical support for hosting and server issues through live chat. Diagnosed database connectivity issues and assisted with system troubleshooting. Maintained documentation of technical issues and resolutions."
           />
         </section>
 
@@ -159,6 +161,12 @@ improvements within the software development lifecycle.
             linkText="Assisto"
             linkUrl="https://assisto.com"
           />
+          <ProjectCard
+            title="AWS Connect (Ongoing Project)"
+            description="Currently learning AWS Amazon Connect and cloud-based contact center technologies, with a focus on IVR, contact flows, call routing, queues, and AWS service integrations."
+            linkText=""
+            linkUrl=""
+          />
         </section>
 
         {/* Certifications Section */}
@@ -185,6 +193,13 @@ improvements within the software development lifecycle.
             imgSrc={devops}
             imgAlt="Udemy course pursuing"
           />
+          <CertCard
+            title="AWS CONNECT"
+            description="Currently Pursuing The AWS CONNECT (Self-Taught)"
+            topicsCovered="Connect Flow,Contact,Channels,Amazon Connect Architecture, Connect Instance,Contact Flow, Flow Blocks,Lambda,Queues,Routing Profile,Prompts and Hours of Operation"
+            imgSrc={Awsconnect}
+            imgAlt="Awsconnect Pursuing"
+          />
         </section>
 
         {/* Education Section */}
@@ -192,15 +207,15 @@ improvements within the software development lifecycle.
           <SectionHeader title="Education" />
           <EducationCard
             institution="The National Institute of Engineering"
-            details="Bachelor of Engineering (B.E) in Information Science | Completed in 2022"
+            details="Bachelor of Engineering (B.E) in Information Science - 2022"
           />
           <EducationCard
             institution="Srimath Polytechnic"
-            details="Diploma in Computer Science | Completed in 2015."
+            details="Diploma in Computer Science - 2015."
           />
           <EducationCard
             institution="Maharshi High School"
-            details="10th Standard (SSLC) Completed in 2009."
+            details="10th Standard (SSLC) - 2009."
           />
         </section>
 
@@ -222,16 +237,20 @@ improvements within the software development lifecycle.
             </div>
             <div className="contact-item" style={{ marginTop: "0.8rem" }}>
               <a
-                href={Tejas_G_Rao_React_Developer_Resume}
-                download="Tejas_G_Rao_React_Developer_Resume.pdf"
+                href={TEJAS_G_RAO_SOFTWARE_ENGINEER_RESUME}
+                download="TEJAS G RAO SOFTWARE_ENGINEER_RESUME.pdf"
                 className="resume-blue-btn"
               >
                 Download Resume
               </a>
-              <a href={Tejas_G_Rao_React_Developer_Resume}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="resume-blue-btn">View Resume</a>
+              <a
+                href={TEJAS_G_RAO_SOFTWARE_ENGINEER_RESUME}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="resume-blue-btn"
+              >
+                View Resume
+              </a>
             </div>
           </div>
         </section>
